7afad969b481e851663771f4c912cad4fd4c4be9
Dmitry-Chernikov <42750167+Dmitry-Chernikov@users.noreply.github.com>
2018-09-14 13:38:59 +0400

    Update AS5048A.h

--- lib/AS5048A/AS5048A.h
+++ lib/AS5048A/AS5048A.h
@@ -72,7 +72,7 @@ class AS5048A{
 	/**
 	* Возвращает инкрементный и декрементный угол поворота в переменную RotationAngle в процедуру прередають адреса переменных 
 	*/
-	void AbsoluteAngleRotation (float *AngleCurrent, float *AnglePrevious)
+	void AbsoluteAngleRotation (float *RotationAngle, float *AngleCurrent, float *AnglePrevious)
 
 
 	/**
